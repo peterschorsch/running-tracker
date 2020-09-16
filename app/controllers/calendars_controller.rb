4bab51fdb10d31a49df336134bfc4154c48f93aa
Peter Schorsch <peteschorsch@gmail.com>
2020-09-16 12:35:55 -0700

    Uncommented Copy Past Weeks Runs method code and added notes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,6 +5,7 @@ class CalendarsController < ApplicationController
 		@run_types = RunType.active_run_types.order_by_name
 	end
 
+	### CREATE DEFAULT RUNS FOR CURRENT WEEK
 	def create_current_week_runs
 		respond_to do |format|
 			if Run.create_weeklong_default_runs(current_user.id)
@@ -15,14 +16,15 @@ class CalendarsController < ApplicationController
 		end
 	end
 
+	### COPY LAST WEEK'S RUNS TO CURRENT WEEK
 	def copy_past_week_runs
-		#respond_to do |format|
+		respond_to do |format|
 			Run.copy_last_weeks_runs(current_user)
-				#format.html { redirect_to request.referrer, notice: "<strong>Last Week's</strong> runs were copy to the current week starting <strong>#{DateTime.now.beginning_of_week.strftime("%B %-d, %Y")}.</strong>" }
-			#else
-				#format.html { redirect_to request.referrer, errors: @usergroup.errors }
-			#end
-		#end
+				format.html { redirect_to request.referrer, notice: "<strong>Last Week's</strong> runs were copy to the current week starting <strong>#{DateTime.now.beginning_of_week.strftime("%B %-d, %Y")}.</strong>" }
+			else
+				format.html { redirect_to request.referrer, errors: @usergroup.errors }
+			end
+		end
 	end
 
 end
