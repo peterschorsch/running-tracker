1299263e28401a298d4cdddeb16e2a16f42c871e
Peter Schorsch <peteschorsch@gmail.com>
2020-09-25 14:44:43 -0700

    Fixed includes statements regarding shoe brand

--- app/controllers/race_results_controller.rb
+++ app/controllers/race_results_controller.rb
@@ -2,8 +2,8 @@ class RaceResultsController < ApplicationController
 	before_action :set_race_run_type
 
 	def index
-		@races = current_user.runs.of_run_type(@race_run_type)
-		@race_results = @races.includes(:gear).order_by_most_recent.group_by_year
+		@races = current_user.runs.of_run_type(@race_run_type).includes(gear: :shoe_brand)
+		@race_results = @races.includes(:state).order_by_most_recent.group_by_year
 
 		@personal_bests = current_user.runs.retrieve_personal_bests
 
