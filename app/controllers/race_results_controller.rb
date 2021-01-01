3025c19ccd4dbe62ff002e506798594bf137e748
Peter Schorsch <peteschorsch@gmail.com>
2020-12-31 16:05:45 -0800

    Added notes to Race Results controller

--- app/controllers/race_results_controller.rb
+++ app/controllers/race_results_controller.rb
@@ -2,13 +2,18 @@ class RaceResultsController < ApplicationController
 	before_action :set_current_user_races, :set_geo_chart_hash
 
 	def index
+		### ALL RACE RESULTS OF CURRENT USER ###
 		@race_results = @races.includes(:state).order_by_most_recent.group_by_year
 
-		@personal_bests = current_user.personal_best_races
-
-		@race_counts = @races.return_race_distance_counts
-
+		### PERSONAL BESTS SECTION ###
+		### COUNTS OF RACE DISTANCES ###
+		@race_distance_counts = @races.return_race_distance_counts
+		### COLUMN WIDTH DEPENDING ON NUMBER DIFFERENT OF RACES DISTANCE ###
+		@rd_counts_column_spacing = 12/@race_distance_counts.count
+		### DATA TO POPULATE MAP OF U.S.A. ###
 		@geo_chart_data = @geo_chart_hash.map{ |key, value| [State.find(key).name, value] }
+		### PERSONAL BESTS ARRAY ###
+		@personal_bests = current_user.personal_best_races
 	end
 
 	private
