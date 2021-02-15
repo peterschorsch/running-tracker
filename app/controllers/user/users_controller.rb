1832f8ee68caf0dc3619f3582e91cbd9fd1b8ad4
Peter Schorsch <peteschorsch@gmail.com>
2021-02-15 14:54:16 -0800

    Fixed User controller check ensuring current_user can only view their related records

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -6,10 +6,10 @@ class User::UsersController < ApplicationController
 
   private
   def check_user_id
-	if current_user.id != params[:user_id].to_i
-		flash[:alert] = "Access Denied."
-		redirect_to dashboards_path
-	end
+    if current_user.id != params[:user_id].to_i
+      flash[:alert] = "Access Denied."
+      redirect_to dashboards_path
+    end
   end
 
 end
